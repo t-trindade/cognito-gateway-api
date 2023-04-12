147b32529a12086ba01457d46b8b701d235d7fa6
talison <talison.souza@flexpag.com>
2023-04-12 19:11:38 -0300

    Gateway API para gerenciamento de aplicações

---
+++ src/main/java/com/flexpag/cognitogatewayapi/CognitoGatewayApiApplication.java
@@ -0,0 +1,11 @@
+package com.flexpag.cognitogatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoGatewayApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CognitoGatewayApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/flexpag/cognitogatewayapi/CognitoGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexpag.cognitogatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
